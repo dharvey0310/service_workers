90f9d4ac009c5f49a658c59a9c2ce4cb2e50ed96
David Harvey <dharvey0310@gmail.com>
2017-06-19 21:56:00 +0100

    console log

--- scripts/index.js
+++ scripts/index.js
@@ -8,19 +8,19 @@ const logInOutButton = document.getElementById('login')
 // define method to get the subscription which returns a promise
 function getSubscription() {
     return navigator.serviceWorker.ready
-    .then(registration => {
-        return registration.pushManager.getSubscription()
-    })
+        .then(registration => {
+            return registration.pushManager.getSubscription()
+        })
 }
 
 // register the service worker and check the initial subscription state
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js')
-    .then(() => {
-        console.log('Service Worker registered')
-        subscriptionButton.removeAttribute('disabled')
-    })
-    .then(setLoginButton)
+        .then(() => {
+            console.log('Service Worker registered')
+            subscriptionButton.removeAttribute('disabled')
+        })
+        .then(setLoginButton)
 }
 
 function login() {
@@ -28,60 +28,61 @@ function login() {
     username = usernameInput.value
     return fetch('https://push-server-testing.herokuapp.com/login', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             username: username
         })
     })
-    .then(getSubscription)
-    .then(subscription => {
-        if (subscription) {
-            console.log('Already registered at ', subscription.endpoint)
-            return fetch('https://push-server-testing.herokuapp.com/register', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    username: username,
-                    endpoint: subscription.endpoint
-                })
-            }).then(setUnsubscribeButton)
-        } else {
-            setSubscribeButton()
-        }
-    })
-    .then(setLogoutButton)
+        .then(getSubscription)
+        .then(subscription => {
+            if (subscription) {
+                console.log('Already registered at ', subscription.endpoint)
+                return fetch('https://push-server-testing.herokuapp.com/register', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        username: username,
+                        endpoint: subscription.endpoint
+                    })
+                }).then(setUnsubscribeButton)
+            } else {
+                setSubscribeButton()
+            }
+        })
+        .then(setLogoutButton)
 }
 
 function logout() {
     return fetch('https://push-server-testing.herokuapp.com/logout', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             username: username
         })
     })
-    .then(setLoginButton)
+        .then(setLoginButton)
 }
 
 // get the 'registration from the service worker and create a new 'subscription'
 // register the subscription with the push message server by sending a POST request to the server
 function subscribe() {
     navigator.serviceWorker.ready
-    .then(registration => {
-        return registration.pushManager.subscribe({userVisibleOnly: true})
-    })
-    .then(subscription => {
-        console.log('Subscribed ', subscription.endpoint)
-        return fetch('https://push-server-testing.herokuapp.com/register', 
-        {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username: username,
-                endpoint: subscription.endpoint
-            })
+        .then(registration => {
+            return registration.pushManager.subscribe({ userVisibleOnly: true })
         })
-    }).then(setUnsubscribeButton)
+        .then(subscription => {
+            console.log('Subscription ', subscription)
+            console.log('Subscribed ', subscription.endpoint)
+            return fetch('https://push-server-testing.herokuapp.com/register',
+                {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        username: username,
+                        endpoint: subscription.endpoint
+                    })
+                })
+        }).then(setUnsubscribeButton)
 }
 
 // get the subscription and unsubscribe it 
@@ -89,17 +90,17 @@ function subscribe() {
 function unsubscribe() {
     getSubscription().then(subscription => {
         return subscription.unsubscribe()
-        .then(() => {
-            console.log('Unsubscribed ', subscription.endpoint)
-            return fetch('https://push-server-testing.herokuapp.com/unregister', 
-            {
-                method: 'POST',
-                headers: {'Content-Type' : 'application/json'},
-                body: JSON.stringify({
-                    endpoint: subscription.endpoint
-                })
+            .then(() => {
+                console.log('Unsubscribed ', subscription.endpoint)
+                return fetch('https://push-server-testing.herokuapp.com/unregister',
+                    {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            endpoint: subscription.endpoint
+                        })
+                    })
             })
-        })
     }).then(setSubscribeButton)
 }
 
